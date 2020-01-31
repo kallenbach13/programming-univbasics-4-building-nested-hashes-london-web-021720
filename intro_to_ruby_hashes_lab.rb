25a1d6c9d6f982e5f19cb85ba800597830d11fd6
Isak Kallenbach <kallenbach13@gmail.com>
2020-01-31 15:28:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,9 +34,7 @@ def monopoly_with_third_tier
 	   :reading_railroad => {},
 	   :pennsylvania_railroad => {},
 	   :b_and_o_railroad => {},
-	   :shortline_railroad => {
-	     "mortage_value" => "$800"
-	   }
+	   :shortline_railroad => {}
 	  }
 	 }
 	}

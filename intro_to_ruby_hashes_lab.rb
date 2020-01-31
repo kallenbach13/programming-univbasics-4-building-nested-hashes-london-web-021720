492d7846516f39f499710e187670ad5eeba9c1bb
Isak Kallenbach <kallenbach13@gmail.com>
2020-01-31 15:20:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,11 +24,7 @@ def monopoly_with_third_tier
   # complexity.
   monopoly = { :railroads => {
 	  :pieces => 4,
-	  :rent_in_dollars => {
-	    :key_one=>
-	    :key_two=>
-	    :key_three=>
-	  }
+	  :rent_in_dollars => {}
 	 }
 	}
 end
